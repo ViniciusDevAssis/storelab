520039b469b44d40d1df47aa1b297bb3736fae66
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2026-03-16 11:19:09 -0300

    início do projeto

---
+++ src/main/java/com/viniciusdevassis/storelab/StorelabApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusdevassis.storelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorelabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorelabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusdevassis/storelab/StorelabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusdevassis.storelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorelabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
